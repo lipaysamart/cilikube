f80acfd76605728a6267f36846144d3e70ef2b05
cillianops <cillianops@gmail.com>
2025-02-27 13:08:42 +0000

    feat: add initial project setup with environment configurations, ESLint, Prettier, and favicon

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -1,30 +1,32 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+<script lang="ts" setup>
+import { h } from "vue"
+import { useTheme } from "@/hooks/useTheme"
+import { ElNotification } from "element-plus"
+// 将 Element Plus 的语言设置为中文
+import zhCn from "element-plus/es/locale/lang/zh-cn"
+
+const { initTheme } = useTheme()
+
+/** 初始化主题 */
+initTheme()
+
+/** 作者小心思 */
+ElNotification({
+  title: "希里安，你好！！",
+  type: "success",
+  offset: 300,
+  message: h(
+    "a",
+    { style: "color: darkblue", target: "_blank", href: "https://www.cillian.website" },
+    "Hello world！点击直达我的博客！记得关注公众号-希里安，一同进步！"
+  ),
+  duration: 0,
+  position: "top-right"
+})
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <el-config-provider :locale="zhCn">
+    <router-view />
+  </el-config-provider>
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
