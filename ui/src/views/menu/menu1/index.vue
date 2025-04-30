58c70733ea67abce29104ca970093724ab39be07
cilliantech <cilliantech@gmail.com>
2025-04-30 14:43:18 +0000

    fix: update Node.js and Go version references in README files and environment configurations

--- ui/src/views/menu/menu1/index.vue
+++ ui/src/views/menu/menu1/index.vue
@@ -2,7 +2,7 @@
   <div class="app-container">
     <h4>
       三级及其以上路由缓存功能默认关闭，需要请前往此配置文件中打开：
-      <el-link type="primary" href="https://github.com/cilliandevops" target="_blank"> src/config/route.ts </el-link>
+      <el-link type="primary" href="https://github.com/cilliantech" target="_blank"> src/config/route.ts </el-link>
     </h4>
     <el-card header="二级路由 - menu1">
       <router-view />
